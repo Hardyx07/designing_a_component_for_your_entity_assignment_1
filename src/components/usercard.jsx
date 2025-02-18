21a7ed7661880805aac8b43a5f68aa0fcd1595c9
Hardyx07 <hardik.tailor@kalvium.community>
2025-02-18 09:44:13 +0530

    task done

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,21 @@
-// write the component code here
+import React from "react";
+
+const UserCard = () => {
+  const ProfilePic = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEg09MmHvC-78aaRxyd52HabsZqI1-u8R6-w&s";
+  const name = "Admin Sir";
+  const email = "admin@login.in";
+  const phone = "6900690069";
+  const address = "Gate no. 1 , IIT Dholakpur , India";
+
+  return (
+    <div className="card">
+      <img src={ProfilePic} alt="Profile Pic" className="logo" />
+      <h2>{name}</h2>
+      <p>{email}</p>
+      <p>{phone}</p>
+      <p>{address}</p>
+    </div>
+  );
+};
+
+export default UserCard;
